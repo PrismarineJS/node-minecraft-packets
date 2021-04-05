118b43436c023b00c09a2d6b96c946674497a4bd
u9g <43508353+u9g@users.noreply.github.com>
2021-04-05 11:05:18 -0400

    Added path.join() instead of relative (#5)

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 
-const DATA_PATH = './minecraft-packets/data'
+const DATA_PATH = path.join(__dirname, '/minecraft-packets/data')
 const gameTypes = fs.readdirSync(DATA_PATH)
 const data = {}
 
@@ -22,15 +22,14 @@ for (const gameType of gameTypes) {
           let [index, type] = file.split('.')
           index-- // decrement so the arrays wont have an empty slot (nmpd starts dumping at packet 1)
           if (file.endsWith('.json')) {
-            fileData = require(path.join(__dirname, DATA_PATH, gameType, version, sendType, packet, file))
+            fileData = require(path.join(DATA_PATH, gameType, version, sendType, packet, file))
           } else if (file.endsWith('.raw')) {
-            fileData = fs.readFileSync(path.join(__dirname, DATA_PATH, gameType, version, sendType, packet, file))
+            fileData = fs.readFileSync(path.join(DATA_PATH, gameType, version, sendType, packet, file))
           }
           if (!data[gameType][version][sendType][packet][index]) {
             data[gameType][version][sendType][packet][index] = {}
           }
           data[gameType][version][sendType][packet][index][type] = fileData
-          // data[gameType][version][sendType][packet][file] = fileData
         }
       }
     }
