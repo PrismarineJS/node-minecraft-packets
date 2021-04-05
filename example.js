18320719cf19ca3291e01caa73294f723c01a7bc
u9g <43508353+u9g@users.noreply.github.com>
2021-04-04 22:33:31 -0400

    add parsing of the files

--- example.js
+++ example.js
@@ -1,3 +1,11 @@
 const packets = require('minecraft-packets')
 
-console.log(packets.pc['1.16']['from-server'].difficulty) // => [ '1.json', '1.raw' ]
+console.log(packets.pc['1.16']['from-server'].difficulty)
+
+// output:
+// [
+//  {
+//    json: { difficulty: 2, difficultyLocked: false },
+//    raw: <Buffer 0d 02 00>
+//  }
+// ]
--- index.js
+++ index.js
@@ -16,7 +16,22 @@ for (const gameType of gameTypes) {
       data[gameType][version][sendType] = {}
       for (const packet of packets) {
         const files = fs.readdirSync(path.join(DATA_PATH, gameType, version, sendType, packet))
-        data[gameType][version][sendType][packet] = files
+        data[gameType][version][sendType][packet] = [] // files
+        for (const file of files) {
+          let fileData = null
+          let [index, type] = file.split('.')
+          index-- // decrement so the arrays wont have an empty slot (nmpd starts dumping at packet 1)
+          if (file.endsWith('.json')) {
+            fileData = require(path.join(__dirname, DATA_PATH, gameType, version, sendType, packet, file))
+          } else if (file.endsWith('.raw')) {
+            fileData = fs.readFileSync(path.join(__dirname, DATA_PATH, gameType, version, sendType, packet, file))
+          }
+          if (!data[gameType][version][sendType][packet][index]) {
+            data[gameType][version][sendType][packet][index] = {}
+          }
+          data[gameType][version][sendType][packet][index][type] = fileData
+          // data[gameType][version][sendType][packet][file] = fileData
+        }
       }
     }
   }
