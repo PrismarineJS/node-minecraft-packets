992dfeaa50f876f1fc7de6534811bdfe0a4d28ac
u9g <43508353+u9g@users.noreply.github.com>
2021-04-07 17:07:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ for (const gameType of gameTypes) {
         const files = fs.readdirSync(path.join(DATA_PATH, gameType, version, sendType, packet))
         data[gameType][version][sendType][packet] = [] // files
         for (const file of files) {
+          if (file.endsWith('.json') || file === 'metadata') {
+            continue
+          }
           let fileData = null
           let [index, type] = file.split('.')
           index-- // decrement so the arrays wont have an empty slot (nmpd starts dumping at packet 1)
